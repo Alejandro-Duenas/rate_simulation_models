c5754b13cf4645f23680b8b16ae00af3ccfc2698
Alejandro Duenas <aduenasg@unal.edu.co>
2021-10-12 19:28:51 -0500

    feat: ended the GBM super class with its graphs
    The GBM object was completed, with the three diferent types of graphs:
      1. Historical+Forecast of the series
      2. Historical Variations
      3. Historial variations + forecasted variations

--- stochastic_modeling.py
+++ stochastic_modeling.py
@@ -6,6 +6,7 @@ the Geometric Brownian Motion Monte Carlo simulation.
 '''
 
 # ------------------------1. Libraries----------------------------------
+from datetime import timedelta
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -140,18 +141,9 @@ class GBMRateSeries(object):
         )
         full_df = pd.concat([full_observed_df, self.sim_df])
         return full_df
-    
-    def get_monthly_variations(self):
-        month_delta = self.series/self.series.shift(1)-1
-        return month_delta
-    
-    def get_annual_variations(self):
-        annual_delta = self.series/self.series.shift(12)-1
-        return annual_delta
 
-    def plot_full_series(self, start, end, historical, 
-                         figsize=(15,10), u_space=1, d_space=-1,
-                         month_space=2):
+    def plot_full_series(self, start, end, figsize=(15,10), month_space=2,
+                         dec=1):
         """Plots the historical and forecasted of the analized seires.
         
         Inputs:
@@ -179,10 +171,6 @@ class GBMRateSeries(object):
         Outputs:
         --------
         """
-        date_max = historical.idxmax()
-        max_value = historical.max()
-        date_min = historical.idxmin()
-        min_value = historical.min()
 
         # Prepare data to be plotted:
         forecast = self.sim_df.agg(['min','max','mean', quant_5,
@@ -190,6 +178,8 @@ class GBMRateSeries(object):
         forecast['Fecha'] = forecast.index
         forecast.reset_index(drop=True, inplace=True)
         series = self.series.to_frame()
+        last_date = series.index.max()
+        last_val = series.loc[last_date].item()
         series_name = series.columns.item()
         series['Fecha'] = series.index
         series.reset_index(drop=True, inplace=True)
@@ -197,6 +187,7 @@ class GBMRateSeries(object):
         
         # Delimit the beginning and end:
         temp = temp[(temp['Fecha']>=start) & (temp['Fecha']<=end)]
+
         # Plot the data:
         fig, ax = plt.subplots(figsize=figsize)
         ax.plot(temp['Fecha'], temp[series_name], 
@@ -245,55 +236,73 @@ class GBMRateSeries(object):
         plt.xticks(rotation=90, ha='right')
 
         # Give label of the last observed value:
+        d_adj = month_space+2
+        local_int = temp.loc[
+            (temp['Fecha']>=last_date-relativedelta(months=d_adj)) &
+            (temp['Fecha']<=last_date+relativedelta(months=int(1.5*d_adj))),
+            series_name
+        ]
         plt.text(
-            x = self.series.index.max()-relativedelta(months=month_space+1),
-            y = self.series[self.series.index.max()]+d_space,
-            s = round(self.series[self.series.index.max()],1),
+            x = last_date-relativedelta(months=d_adj),
+            y = local_int.max(),
+            s = round(last_val,dec),
             color = self.COLORS['hist']
         )
-        for date in forecast.loc[forecast['Fecha']<=end,'Fecha']:
-            if date.month%6==0:
-                
-                temp_min = round(temp.loc[temp['Fecha']==date, 'min'].item(),1)
-                temp_max = round(temp.loc[temp['Fecha']==date, 'max'].item(),1)
-                temp_mean = round(temp.loc[temp['Fecha']==date, 'mean'].item(),1)
-                temp_95 = round(temp.loc[temp['Fecha']==date, 'quant_95'].item(),1)
-                temp_5 = round(temp.loc[temp['Fecha']==date, 'quant_5'].item(),1)
+
+        dates = temp.loc[temp['Fecha']>last_date, 'Fecha']
+        dist_u = 1.01
+        dist_d = 0.96
+        for date in dates:
+            if date.month%6 ==0:
+                temp_min = round(temp.loc[temp['Fecha']==date, 'min'].item(),dec)
+                temp_max = round(temp.loc[temp['Fecha']==date, 'max'].item(),dec)
+                temp_mean = round(temp.loc[temp['Fecha']==date, 'mean'].item(),dec)
+                temp_95 = round(temp.loc[temp['Fecha']==date, 'quant_95'].item(),dec)
+                temp_5 = round(temp.loc[temp['Fecha']==date, 'quant_5'].item(),dec)
+                local_int = temp.loc[
+                    (temp['Fecha']>=date-relativedelta(months=month_space)) &
+                    (temp['Fecha']<=date+relativedelta(months=int(1.5*month_space)))
+                ]
+                pos_values = local_int.max()
                 plt.text(
                     x = date-relativedelta(months=month_space), 
-                    y = temp_min+d_space,
+                    y = pos_values['min']*dist_d,
                     s = temp_min,
                     color = self.COLORS['min']
                 )
                 plt.text(
                     x = date-relativedelta(months=month_space), 
-                    y = temp_max+u_space,
+                    y = pos_values['max']*dist_u,
                     s = temp_max,
                     color = self.COLORS['max']
                 )
                 plt.text(
                     x = date-relativedelta(months=month_space), 
-                    y = temp_mean+0.3*u_space,
+                    y = pos_values['mean']*dist_u,
                     s = temp_mean,
                     color = self.COLORS['mean']
                 )
                 plt.text(
                     x = date-relativedelta(months=month_space), 
-                    y = temp_95+u_space*0.7,
+                    y = pos_values['quant_95']*dist_u,
                     s = temp_95,
                     color = self.COLORS['perc_95']
                 )
                 plt.text(
                     x = date-relativedelta(months=month_space), 
-                    y = temp_5+d_space,
+                    y = pos_values['quant_5']*dist_d,
                     s = temp_5,
                     color = self.COLORS['perc_5']
                 )
+        bottom_val = temp.select_dtypes(include='float64').min().min()
+        top_val = temp.select_dtypes(include='float64').max().max()
+        if bottom_val<0:
+            bottom_val = bottom_val*1.1
+        else:
+            bottom_val = bottom_val*0.9
         plt.ylim(
-            bottom = temp.select_dtypes(include='float64').min().min()\
-                +1.5*d_space, 
-            top = temp.select_dtypes(include='float64').max().max()\
-                +1.5*u_space
+            bottom = bottom_val, 
+            top = top_val*1.1
         )
         plt.xlim(
             left = temp['Fecha'].min(), 
@@ -301,6 +310,275 @@ class GBMRateSeries(object):
         )
         plt.legend()
         return fig, ax
+    
+    def plot_historic_variation(self, start='2000-01', ref=1, 
+                                figsize=(15,10), month_space=2, dec=2):
+        series = (self.series-self.series.shift(ref))[start:]
+        hist_max = series.max()
+        max_date = series.idxmax()
+        hist_min = series.min()
+        min_date = series.idxmin()
+        last_date = series.index.max()
+        start_date = series.index.min()
+        series_name = series.name
+        series = series.to_frame()
+        series['Fecha'] = series.index
+        series.reset_index(drop=True, inplace=True)
+
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.plot(
+            series['Fecha'], 
+            series[series_name],
+            color = self.COLORS['hist'],
+            label = 'Variación'
+        )
+        ax.axhline(hist_max, color=self.COLORS['hist_max'], 
+                   linestyle=':', linewidth=2, label='Min - Max')
+        ax.axhline(hist_min, color=self.COLORS['hist_min'], 
+                   linestyle=':', linewidth=2)
+        month_fmt = mdates.MonthLocator(interval=12)
+        ax.xaxis.set_major_locator(month_fmt)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%y'))
+        plt.xticks(rotation=90, ha='right')
+        plt.text(
+            x = start_date-relativedelta(months=month_space),
+            y = hist_max*1.01, 
+            s = f"{max_date.strftime('%b-%y')}: {round(hist_max,dec)}",
+            color = self.COLORS['hist_max']
+        )
+        plt.text(
+            x = start_date-relativedelta(months=month_space),
+            y = hist_min*1.1, 
+            s = f"{min_date.strftime('%b-%y')}: {round(hist_min,dec)}",
+            color = self.COLORS['hist_min']
+        )
+        for date in series['Fecha']:
+            if date.month==last_date.month:
+                local_int = series.loc[
+                    (series['Fecha']>=date-relativedelta(months=month_space)) &
+                    (series['Fecha']<=date+relativedelta(months=2*month_space)),
+                    series_name
+                    ]
+                val = series.loc[series['Fecha']==date, series_name].item()
+                if val>0 :
+                    plt.text(
+                        x = date-relativedelta(months=month_space),
+                        y = local_int.max()*1.05,
+                        s = round(val, dec),
+                        color = self.COLORS['hist']
+                    )
+                else:
+                    plt.text(
+                        x = date-relativedelta(months=month_space),
+                        y = local_int.min()*1.1,
+                        s = round(val, dec),
+                        color = self.COLORS['hist']
+                    )
+
+        plt.xlim(
+            left = start_date-relativedelta(months=month_space),
+            right = last_date+relativedelta(months=month_space)
+        )
+        plt.ylim(
+            bottom = hist_min*1.2,
+            top = hist_max*1.2
+        )
+        plt.legend()
+        return fig, ax
+
+    def plot_full_variations(self, start, end, ref=1, figsize=(15,10), 
+                             month_space=2, dec=2):
+        # Get variations:
+        var_series = (self.series-self.series.shift(ref)).dropna()
+        # var_series = np.log(self.series/self.series.shift(ref))
+        series_name = var_series.name
+        var_forecast = self.get_full_df()-self.get_full_df().shift(ref)
+        # var_forecast = np.log(self.get_full_df()/self.get_full_df().shift(ref))
+        var_forecast = var_forecast.loc[var_series.index.max():]
+        var_forecast =  var_forecast.agg(
+            ['min','max','mean',quant_5, quant_95],
+            axis = 1
+        )
+
+        # Get important variables
+        var_series = var_series[start:]
+        hist_max = var_series.max()
+        max_date = var_series.idxmax()
+        hist_min = var_series.min()
+        min_date = var_series.idxmin()
+        last_date = var_series.index[-1]
+        start_date = var_series.index[0]
+        last_obs_val = var_series.last('1D').item()
+
+        # Give the correct format and join the full information of variations:
+        var_series = var_series.to_frame()
+        var_series['Fecha'] = var_series.index
+        var_series.reset_index(drop=True, inplace=True)
+        var_forecast['Fecha'] = var_forecast.index
+        var_forecast.reset_index(drop=True, inplace=True)
+        full_var = var_series.merge(var_forecast, how='outer', on='Fecha')
+
+        # Delimit the data's beginning and end:
+        full_var = full_var[
+            (full_var['Fecha']>=start) &
+            (full_var['Fecha']<=end)
+        ]
+
+        # Plot the data:
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.plot(full_var['Fecha'], full_var[series_name],
+                color = self.COLORS['hist'])
+        ax.plot(
+            full_var['Fecha'], 
+            full_var['quant_5'], 
+            color = self.COLORS['perc_5'],
+            linestyle = ':',
+            linewidth = 2,
+            label = 'Perc 5 - Perc 95'
+        )
+        ax.plot(
+            full_var['Fecha'], 
+            full_var['quant_95'], 
+            color = self.COLORS['perc_95'],
+            linestyle = ':',
+            linewidth = 2
+        )
+        ax.plot(
+            full_var['Fecha'], 
+            full_var['min'], 
+            color = self.COLORS['min'],
+            linestyle = '--',
+            linewidth = 2,
+            label = 'Min - Max'
+        )
+        ax.plot(
+            full_var['Fecha'], 
+            full_var['max'], 
+            color = self.COLORS['max'],
+            linestyle = '--',
+            linewidth = 2
+        )
+        ax.plot(
+            full_var['Fecha'], 
+            full_var['mean'], 
+            color = self.COLORS['mean'],
+            linewidth = 2,
+            label = 'Media'
+        )
+        ax.axhline(hist_max, color=self.COLORS['hist_max'], 
+                   linestyle=':', linewidth=2, label='Min - Max')
+        ax.axhline(hist_min, color=self.COLORS['hist_min'], 
+                   linestyle=':', linewidth=2)
+        
+        # Set the axis values:
+        month_fmt = mdates.MonthLocator(interval=3)
+        ax.xaxis.set_major_locator(month_fmt)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%y'))
+        plt.xticks(rotation=90, ha='right')
+
+        # Give label of the reference values:
+        da = month_space+1
+        local_int = full_var.loc[
+            (full_var['Fecha']>=last_date-relativedelta(months=da)) &
+            (full_var['Fecha']<=last_date+relativedelta(months=int(1.5*da))),
+            series_name
+        ]
+        plt.text(
+            x = last_date-relativedelta(months=da),
+            y = 1.05*local_int.max(),
+            s = round(last_obs_val,dec),
+            color = self.COLORS['hist']
+        )
+        plt.text(
+            x = start_date,
+            y = hist_max*1.01, 
+            s = f"{max_date.strftime('%b-%y')}: {round(hist_max,dec)}",
+            color = self.COLORS['hist_max']
+        )
+        plt.text(
+            x = start_date,
+            y = hist_min*1.1, 
+            s = f"{min_date.strftime('%b-%y')}: {round(hist_min,dec)}",
+            color = self.COLORS['hist_min']
+        )
+
+        dates = full_var.loc[full_var['Fecha']>last_date, 'Fecha']
+        dist_u = 1.01
+        dist_d = 0.96
+        for date in dates:
+            if date.month%6 == 0:
+                temp_min = round(
+                    full_var.loc[full_var['Fecha']==date, 'min'].item(),
+                    dec
+                )
+                temp_max = round(
+                    full_var.loc[full_var['Fecha']==date, 'max'].item(),
+                    dec
+                )
+                temp_mean = round(
+                    full_var.loc[full_var['Fecha']==date, 'mean'].item(),
+                    dec
+                )
+                temp_95 = round(
+                    full_var.loc[full_var['Fecha']==date, 'quant_95'].item(),
+                    dec
+                )
+                temp_5 = round(
+                    full_var.loc[full_var['Fecha']==date, 'quant_5'].item(),
+                    dec
+                )
+                local_int = full_var.loc[
+                    (full_var['Fecha']>=date-relativedelta(months=month_space)) &
+                    (full_var['Fecha']<=date+relativedelta(months=int(1.5*month_space)))
+                ]
+                pos_values = local_int.max()
+                plt.text(
+                    x = date-relativedelta(months=month_space), 
+                    y = pos_values['min']*dist_d,
+                    s = temp_min,
+                    color = self.COLORS['min']
+                )
+                plt.text(
+                    x = date-relativedelta(months=month_space), 
+                    y = pos_values['max']*dist_u,
+                    s = temp_max,
+                    color = self.COLORS['max']
+                )
+                plt.text(
+                    x = date-relativedelta(months=month_space), 
+                    y = pos_values['mean']*dist_u,
+                    s = temp_mean,
+                    color = self.COLORS['mean']
+                )
+                plt.text(
+                    x = date-relativedelta(months=month_space), 
+                    y = pos_values['quant_95']*dist_u,
+                    s = temp_95,
+                    color = self.COLORS['perc_95']
+                )
+                plt.text(
+                    x = date-relativedelta(months=month_space), 
+                    y = pos_values['quant_5']*dist_d,
+                    s = temp_5,
+                    color = self.COLORS['perc_5']
+                )
+        bottom_val = full_var.select_dtypes(include='float64').min().min()
+        top_val = full_var.select_dtypes(include='float64').max().max()
+        if bottom_val<0:
+            bottom_val = bottom_val*1.1
+        else:
+            bottom_val = bottom_val*0.9
+        plt.ylim(
+            bottom = bottom_val, 
+            top = top_val*1.1
+        )
+        plt.xlim(
+            left = full_var['Fecha'].min(), 
+            right = full_var['Fecha'].max()+relativedelta(months=month_space)
+        )
+        plt.legend()
+        return fig, ax
+
 
 
 #-----------------------3. Auxiliary Functions -------------------------
@@ -309,6 +587,4 @@ def quant_5(x):
 
 def quant_95(x):
     return x.quantile(0.95)
-
-
 
